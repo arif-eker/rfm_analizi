87a034a690562594f37d81330f210c31d8a53369
Arif Eker <eng.arifeker@gmail.com>
2020-12-11 21:18:02 +0300

    recency işlemleri yapıldı.

--- rfm_analizi.py
+++ rfm_analizi.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import datetime as dt
 
 # bütün gözlemleri ve değişkenleri (satır ve sütunlar) görebilmek için :
 pd.set_option('display.max_columns', None);
@@ -42,3 +43,68 @@ df.columns
 # şimdi bu değişkenlerin bazılarında kaç tane eşsiz (birbirinden farklı) değer olduğuna bakalım :
 print("Description : ", df["Description"].nunique())
 print("StockCode : ", df["StockCode"].nunique())
+
+# bu değişkenlerde toplamda kaç adet eleman (değer) olduğuna bakalım
+
+print("Description : ", df["Description"].value_counts().sum())
+print("StockCode : ", df["StockCode"].value_counts().sum())
+
+# RFM ANALİZİNE GEÇELİM
+
+# öncelikle eksik gözlemlerimizi silelim. eksik verileri doldurmanın çeşitli yolları var ama biz uzatmamak için siliyoruz.
+# bu elbette bazı yan etkilere sahip olacaktır ama şuan bunu düşünmemize gerek yok.
+
+pure_df = df.dropna()
+
+# eksik gözlemler silinmiş mi ?
+pure_df.isnull().values.any()
+
+# eksik gözlem yok, peki değişkenlerin kaç adet elemanları kaldı, sağlaması?
+pure_df.isnull().sum()
+
+# Bu veri setindeki faturada C : geri iade edilen faturayı temsil eder. Yani negatif işleme girer. Biz bu değerleri de çıkarıyoruz :
+pure_df = pure_df[~pure_df["Invoice"].str.contains("C", na=False)]
+
+# elimizde kaç adet gözlem birimi var ?
+pure_df.shape
+
+# RFM için maksimum gelir lazım olacak bu yüzden TotalPrice değişkeni oluşturacağız :
+pure_df["TotalPrice"] = pure_df["Quantity"] * pure_df["Price"]
+
+# biraz verimizi inceleyelim. bunun için önceki haline ve şuanki haline bakabiliriz.
+
+# önceki:
+df.describe().T
+
+# şimdiki
+pure_df.describe().T
+
+"""
+Recency : Müşterinin son satın almasından bugüne kadar geçen süre
+
+Frequency : Toplam satın alma sayısı
+
+Monetary : Müşterinin yaptığı toplam harcama
+"""
+
+# Veri setinin en son ne zaman veri tuttuğuna bakalım. Şuan Tarih 2020 fakat biz bu tarihi baz alırsak çok yanlış yaparız :)
+pure_df["InvoiceDate"].max()
+
+# burada görüyoruz ki son tarih 2011 yılının, 12. ayının 9. günü.
+# bu yüzden biz de aynı tarihlerin bir gün sonrasını alalım yani 10. günü ki güzel bir sonuç alalım.
+last_time = dt.datetime(2011, 12, 10)
+
+#  Müşterilerin son satın alımlarından itibaren, son tarihe göre kaç gün geçmiş bakalım :
+# önce geçici df oluşturalım
+temp_df = (last_time - pure_df.groupby("Customer ID").agg({"InvoiceDate": "max"}))
+temp_df.head()
+
+# InvoiceDate değişkenini yeniden isimlendirelim :
+temp_df.rename(columns={"InvoiceDate": "Recency"}, inplace=True)
+temp_df.head()
+
+# recency df oluşturalım ve bu df, ek saat ve dakikalrı içermesin
+# burada geçici dfteki, recency değişkeninin her bir elemana eriştik, bunların günlerini aldık.
+
+recency_df = temp_df["Recency"].apply(lambda x: x.days)
+recency_df.head()
